440936d1b4915c12ffddf208b3ce3826aec05e86
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2025-12-20 20:07:34 +0700

    feat: Added jelajah screen and book item widget

---
+++ lib/presentation/screens/home/jelajah.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JelajahScreen extends StatelessWidget {
+  const JelajahScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
