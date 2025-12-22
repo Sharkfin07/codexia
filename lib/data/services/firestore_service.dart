1e0da6fbf291cf091f89193b896c7dc96b5459d1
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2025-12-22 23:42:26 +0700

    feat: Add set document functionality to the FirestoreService

--- lib/data/services/firestore_service.dart
+++ lib/data/services/firestore_service.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
+  FirestoreService._();
+  static final FirestoreService instance = FirestoreService._();
+
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   CollectionReference<Map<String, dynamic>> collection(String path) =>
       _db.collection(path);
+
+  DocumentReference<Map<String, dynamic>> doc(String path) => _db.doc(path);
 
   FieldValue get serverTimestamp => FieldValue.serverTimestamp();
 
@@ -18,6 +23,7 @@ class FirestoreService {
     return copy;
   }
 
+  // * Add Functionality
   Future<DocumentReference<Map<String, dynamic>>> add(
     String collectionPath,
     Map<String, dynamic> data, {
@@ -26,5 +32,18 @@ class FirestoreService {
     return collection(
       collectionPath,
     ).add(_withServerTimestamps(data, setCreatedAt: setCreatedAt));
+  }
+
+  // * Set Functionality
+  Future<void> set(
+    String docPath,
+    Map<String, dynamic> data, {
+    bool merge = false,
+    bool setCreatedAt = false,
+  }) {
+    return doc(docPath).set(
+      _withServerTimestamps(data, setCreatedAt: setCreatedAt),
+      SetOptions(merge: merge),
+    );
   }
 }
