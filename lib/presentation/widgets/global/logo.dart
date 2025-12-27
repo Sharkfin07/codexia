2a753209d68cde9c88cb24e673ff15a3005ea612
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2025-12-27 22:33:29 +0700

    feat: Add home screen

--- lib/presentation/widgets/global/logo.dart
+++ lib/presentation/widgets/global/logo.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 class LogoRegular extends StatelessWidget {
   final double? width;
-  const LogoRegular({super.key, this.width = 35});
+  final bool forceDark;
+  const LogoRegular({super.key, this.width = 35, this.forceDark = false});
 
   @override
   Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final isDark = forceDark
+        ? true
+        : Theme.of(context).brightness == Brightness.dark;
     final imgPath =
         'assets/images/codexia-logo-${isDark ? 'dark' : 'light'}.png';
     return Image.asset(imgPath, width: width);
