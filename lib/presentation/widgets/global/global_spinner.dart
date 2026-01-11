8513e84677c2cfd169273eba07e2ca6b54b09887
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-01-11 20:47:23 +0700

    feat(animation): Implemented loading animation from Lottie

--- lib/presentation/widgets/global/global_spinner.dart
+++ lib/presentation/widgets/global/global_spinner.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:lottie/lottie.dart';
 
-class GlobalSpinner extends StatelessWidget {
+class GlobalSpinner extends StatefulWidget {
   const GlobalSpinner({super.key, this.size = 64});
 
   final double size;
 
   @override
+  State<GlobalSpinner> createState() => _GlobalSpinnerState();
+}
+
+class _GlobalSpinnerState extends State<GlobalSpinner>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: size,
-      height: size,
-      child: const RiveAnimation.asset(
-        'assets/animations/circle-spinner-animation-loading-animation.riv',
-        fit: BoxFit.contain,
+      width: widget.size,
+      height: widget.size,
+      child: Lottie.asset(
+        'assets/animations/loading_lottie.json',
+        controller: _controller,
+        onLoaded: (composition) {
+          _controller
+            ..duration = const Duration(milliseconds: 600)
+            ..forward();
+        },
       ),
     );
   }
